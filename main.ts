3c32865edac29217fb7411dcaa7b968482cf87bd
lebiosphere <lebiosphere@gmail.com>
2024-05-20 12:28:32 +0200

    ajustement du temps entre sauts

--- main.ts
+++ main.ts
@@ -1,13 +1,22 @@
 input.onButtonPressed(Button.A, function () {
-    sauts = 0
-    basic.pause(200)
+    basic.showNumber(sauts)
 })
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Fabulous)
+    basic.showNumber(max)
+})
+let max = 0
 let sauts = 0
 basic.showIcon(IconNames.Rabbit)
+sauts = 0
+let min = 10000
+max = 0
+basic.pause(200)
 basic.forever(function () {
-    if (input.acceleration(Dimension.Strength) > 700) {
+    if (input.acceleration(Dimension.Strength) > 2000) {
         sauts += 1
         basic.pause(150)
-        basic.showNumber(sauts)
     }
+    min = Math.min(min, input.acceleration(Dimension.Strength))
+    max = Math.max(max, input.acceleration(Dimension.Strength))
 })
